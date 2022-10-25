9823b70ca82b8debc07e47b81738e8c9044d18e9
yaswanth-vakkala <yaswanthv87@gmail.com>
2022-10-26 00:17:50 +0530

    complete homepage product listing

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const Header = () => {
   return (
@@ -13,8 +12,12 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
-              <Nav.Link href="/login"><i className='fas fa-user'></i>Login</Nav.Link>
+              <Nav.Link href="/cart">
+                <i className="fas fa-shopping-cart"></i>Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <i className="fas fa-user"></i>Login
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
